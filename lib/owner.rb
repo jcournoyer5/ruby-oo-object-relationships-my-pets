d59bd10de34271872579ac533925e24b5195fd97
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-17 20:09:01 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name
+  
+  def initialize(name)
+      @name = name
+      @species = human
+  end      
+  
+  def say_species
+  
+  end  
+  
+  
 end
