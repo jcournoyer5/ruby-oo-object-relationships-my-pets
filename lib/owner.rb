363e80df8f7bbdc3815fab6a0385bea946c16810
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-19 02:37:53 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,20 +57,58 @@ class Owner
   end
   
   def walk_dogs
-    
-    dog.mood.self = "happy"
+  # dog.mood.self = "happy"
+    self.dogs.each {|dog| dog.mood = "happy"}
   end  
   
   def feed_cats
-  cat.mood.self = "happy"
+    self.cats.each {|cat| cat.mood = "happy"}
+  # cat.mood.self = "happy"
   end
   
   def sell_pets
-  
+   self.
   end 
   
-  def list_pets
+    #   describe "#sell_pets" do
+    #   it 'can sell all its pets, which makes them nervous' do
+    #     fido = Dog.new("Fido", @owner)
+    #     tabby = Cat.new("Tabby", @owner)
+
+    #     [fido, tabby].each {|o| o.mood = "happy" }
+
+    #     @owner.sell_pets
+
+    #     [fido, tabby].each { |o| expect(o.mood).to eq("nervous") }
+    #   end
+
+    #   it 'can sell all its pets, which leaves them without an owner' do
+    #     fido = Dog.new("Fido", @owner)
+    #     tabby = Cat.new("Tabby", @owner)
+
+    #     [fido, tabby].each {|o| o.mood = "happy" }
+
+    #     @owner.sell_pets
+
+    #     [fido, tabby].each { |o| expect(o.owner).to be(nil) }
+    #     expect(@owner.cats.count).to eq(0)
+    #     expect(@owner.dogs.count).to eq(0)
+    #   end
+    # end
+
   
+  def list_pets
+    "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
   end  
   
+      # describe "#list_pets" do
+      # it 'can list off its pets' do
+      #   @owner.buy_cat("Crookshanks")
+      #   @owner.buy_cat("Fido")
+      #   @owner.buy_dog("Snuffles")
+      #   @owner.buy_dog("Charley")
+      #   expect(@owner.list_pets).to eq("I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s).")
+      # end
+  
+  end
 end
